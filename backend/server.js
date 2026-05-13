0b011ff5b744722ff6276eb04449825000b486f0
Sandhya <aapka-email@gmail.com>
2026-05-13 14:29:02 +0530

    Fix CORS typo

--- backend/server.js
+++ backend/server.js
@@ -42,23 +42,18 @@ connectDB();
 if (!fs.existsSync('./uploads')) fs.mkdirSync('./uploads');
 
 // Middleware
-const allowedOrigins = [
-  'http://localhost:5173',
-  'http://localhost:5174',
-  'http://localhost:5175',
-  process.env.FRONTEND_URL
-];
+
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'http://localhost:5175',
+    process.env.FRONTEND_URL
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true
 }));
+
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
