42aaea5c44d2ac45c8320c4481e8b920fb92de80
Sandhya <aapka-email@gmail.com>
2026-05-13 15:10:24 +0530

    Fix mongoose next error

--- backend/models/User.js
+++ backend/models/User.js
@@ -32,25 +32,15 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-// HASH PASSWORD BEFORE SAVE
-userSchema.pre('save', async function(next) {
+// HASH PASSWORD
+userSchema.pre('save', async function () {
 
-  if (!this.isModified('password')) {
-    return next();
-  }
+  if (!this.isModified('password')) return;
 
-  try {
+  const salt = await bcrypt.genSalt(10);
 
-    const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 
-    this.password = await bcrypt.hash(this.password, salt);
-
-    next();
-
-  } catch (error) {
-
-    next(error);
-  }
 });
 
 // COMPARE PASSWORD
